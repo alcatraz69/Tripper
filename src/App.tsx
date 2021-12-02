5d93557ff144615e325758eed9d5164e0c5da690
Premsai K <prem@Prems-MacBook-Pro.local>
2021-12-02 18:49:16 +0530

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        welcome to tripper!
-      </header>
+      <Header/>
     </div>
   );
 }
